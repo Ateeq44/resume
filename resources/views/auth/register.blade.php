be934eaafb38a8353ec59ce1376dc39a0de51fb0
ateeq44 <ateeqrehman4479@gmail.com>
2025-12-18 01:21:58 +0500

    fdsa

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
 <section class="vh-100">
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+      <div class="col-sm-12 col-md-8 col-lg-6 col-xl-5">
         <div class="card shadow-2-strong" style="border-radius: 1rem;">
           <div class="card-body p-5">
             <div class="d-flex justify-content-center mb-4">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,10 +18,10 @@
 </div>
 <div class="container p-60">
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-6 px-4">
 
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 px-4">
             <h1 class="text-dark">Build Your Resume Anywhere with Our Mobile-Friendly Builder</h1>
             <p  class="text-dark">Create a professional resume in minutes with Resume Maker App, a fully responsive tool. Simply enter your work experience, skills, and career goals from any smartphone, tablet, or desktop browser, and instantly generate a polished, ATS-friendly resume ready to send.</p>
             <a href="{{ url('login') }}" class="btn btn-success">Build My Resume Now</a>
@@ -30,13 +30,13 @@
 </div>
 <div class="container p-60">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-12 px-4">
             <h2>What’s Included in Resume Maker App’s Free Plan</h2>
             <p>Our free plan is one of the most generous among resume builders. Here’s what you get:</p>
         </div>
     </div>
     <div class="row row-cols-1 row-cols-md-2 g-4 mt-3">
-        <div class="col">
+        <div class="col px-4">
             <div class="card">
                 <div class="card-body">
                 <img style="width: 50px;" src="{{asset('images/g.png')}}" class="card-img-top mb-3" alt="{{asset('images/g.png')}}">
@@ -45,7 +45,7 @@
                 </div>
             </div>
         </div>
-        <div class="col">
+        <div class="col px-4">
             <div class="card">
                 <div class="card-body">
                 <img style="width: 50px;" src="{{asset('images/3.png')}}" class="card-img-top mb-3" alt="{{asset('images/3.png')}}">
@@ -54,7 +54,7 @@
                 </div>
             </div>
         </div>
-        <div class="col">
+        <div class="col px-4">
             <div class="card">
                 <div class="card-body">
                 <img style="width: 50px;" src="{{asset('images/2.png')}}" class="card-img-top mb-3" alt="{{asset('images/2.png')}}">
@@ -63,7 +63,7 @@
                 </div>
             </div>
         </div>
-        <div class="col">
+        <div class="col px-4">
             <div class="card">
                 <div class="card-body">
                 <img style="width: 50px;" src="{{asset('images/4.png')}}" class="card-img-top mb-3" alt="{{asset('images/4.png')}}">
